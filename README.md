05c17da3c0f4495dd9ca821884139dc9eb4d5373
Mads Røskar <madshvero@gmail.com>
2017-10-26 18:08:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# create-dummy-image
+
+This is a very simple tool to create a dummy image, which is written to a file. It uses <https://dummyimage.com> to create the images.
+
+## Installation
+
+`npm install -g create-dummy-image`
+
+## Usage
+
+`create-dummy-image <options>`
+
+```
+Options:
+  --help                   Show help                                   [boolean]
+  --version                Show version number                         [boolean]
+  --width, -w                                                    [default: "32"]
+  --height, --32                                                 [default: "32"]
+  --text, -t                                                      [default: "C"]
+  --backgroundColor, --bg                                       [default: "000"]
+  --foregroundColor, --fg                                       [default: "fff"]
+  --output, -o                                          [default: "favicon.ico"]
+```
+
---
+++ index.js
@@ -0,0 +1,68 @@
+#!/usr/bin/env node
+
+var http = require('https');
+var fs = require('fs');
+const yargs = require('yargs');
+var currentFolderName = require('path').basename(process.cwd());
+var Promise = require('promise');
+
+var argv = yargs
+    .option('width', {
+        alias: 'w',
+        default: '32'
+    })
+    .option('height', {
+        alias: '32',
+        default: '32'
+    })
+    .option('text', {
+        alias: 't',
+        default: currentFolderName
+            .substr(0, 1)
+            .toUpperCase()
+    })
+    .option('backgroundColor', {
+        alias: 'bg',
+        default: '000'
+    })
+    .option('foregroundColor', {
+        alias: 'fg',
+        default: 'fff'
+    })
+    .option('output', {
+        alias: 'o',
+        default: 'favicon.ico'
+    }).argv;
+
+var file = fs.createWriteStream(argv.output);
+var request = http.get(generateUrl(), function (response) {
+    if (response.statusCode === 200) {
+        response.pipe(file);
+    }
+});
+
+request.on("error", function (error) {
+    file.close();
+    fs.unlink(dest, function () {});
+    console.log("An error occurred: ", error.message)
+});
+
+file.on("error", function (error) {
+    file.close();
+
+    if (error.code === "EEXIST") {
+        console.log("File already exists");
+    } else {
+        fs.unlink(dest, function () {});
+        console.log("An error occurred: ", error.message);
+    }
+});
+
+file.on("finish", function () {
+    file.close();
+    console.log('Created file:', argv.output);
+});
+
+function generateUrl() {
+    return 'https://dummyimage.com/' + argv.width + 'x' + argv.height + '/' + argv.backgroundColor + '/' + argv.foregroundColor + '&text=' + argv.text;
+}
